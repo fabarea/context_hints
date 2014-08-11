8c01f915b4e380624d73e3e7df665e6962bc1a6f
Fabien Udriot <fabien.udriot@ecodev.ch>
2014-08-11 17:30:47 +0200

    [TASK] Clean up ext_emconf

--- ext_emconf.php
+++ ext_emconf.php
@@ -15,18 +15,9 @@ $EM_CONF[$_EXTKEY] = array (
   'category' => 'be',
   'author' => 'Fabien Udriot',
   'author_email' => 'fabien.udriot@typo3.org',
-  'shy' => '',
-  'priority' => '',
-  'module' => '',
   'state' => 'stable',
-  'internal' => '',
-  'uploadfolder' => '0',
-  'createDirs' => '',
-  'modify_tables' => '',
   'clearCacheOnLoad' => 0,
-  'lockType' => '',
   'version' => '1.0.0',
-  'constraints' =>
   array (
     'depends' =>
     array (
